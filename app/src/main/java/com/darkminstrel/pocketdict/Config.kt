df77e94dc963677d2f793e686877cafd723b688b
Dark <darkminstrel@mail.ru>
2020-03-30 23:06:27 +0300

    small fixes

--- app/src/main/java/com/darkminstrel/pocketdict/Config.kt
+++ app/src/main/java/com/darkminstrel/pocketdict/Config.kt
@@ -2,4 +2,5 @@ package com.darkminstrel.pocketdict
 
 object Config {
     const val ENABLE_IME_INSETS = false
+    const val MEMORY_CACHE_SIZE = 50    //translations
 }
--- app/src/main/java/com/darkminstrel/pocketdict/ui/frg_list/AdapterFavorites.kt
+++ app/src/main/java/com/darkminstrel/pocketdict/ui/frg_list/AdapterFavorites.kt
@@ -4,6 +4,7 @@ import android.util.LruCache
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.darkminstrel.pocketdict.Config
 import com.darkminstrel.pocketdict.R
 import com.darkminstrel.pocketdict.colorize
 import com.darkminstrel.pocketdict.data.ParsedTranslation
@@ -17,7 +18,7 @@ class AdapterFavorites(private val vm: FrgListViewModel, private val onClickList
     private val allKeys = ArrayList<String>()
     private val filteredKeys = ArrayList<String>()
     private val jobsMap = IdentityHashMap<ViewHolderTextPair, Job>()
-    private val cache = LruCache<String, ParsedTranslation>(30)
+    private val cache = LruCache<String, ParsedTranslation>(Config.MEMORY_CACHE_SIZE)
 
     fun setKeys(keys:List<String>){
         this.allKeys.clear()
